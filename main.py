86eb01847f6eac0a900f2282c738eaca893d3eca
eli-requioma <eli.requioma@gmail.com>
2025-09-26 18:39:44 +0800

    Finished Pygame Window Setup

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
+
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(60)
+        pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
