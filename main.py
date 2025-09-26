67a35536e9f45436bc5f642122966f6584349b9f
Sera <b2028sllopez@pshs.edu.ph>
2025-09-26 18:57:41 +0800

    FInished Creating Plaents

--- main.py
+++ main.py
@@ -6,6 +6,34 @@ WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
+WHITE = (255,255,255)
+YELLOW = (255, 255, 0)
+
+class Planet:
+    AU = 149.6e6 * 1000 #distance from sun in meters
+    G = 6.67426e-11
+    SCALE = 250 / AU #1 AU = 100 px
+    TIMESTEP = 3600 * 24 # 1 day
+
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass #in kg
+
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH/2
+        y = self.y * self.SCALE + HEIGHT/2
+
+        pygame.draw.circle(win, self.color, (x,y), self.radius)
 
 def main():
     run = True
@@ -22,4 +50,3 @@ def main():
     pygame.quit()
 
 main()
-#sera was here
